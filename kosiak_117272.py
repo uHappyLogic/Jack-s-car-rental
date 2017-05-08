943e7684be05b9721fcc4855be2a1e6cabdc9404
uHappyLogic <kosiak.lukasz@gmail.com>
2017-05-08 22:44:52 +0200

    Nearly correct solution

--- kosiak_117272.py
+++ kosiak_117272.py
@@ -5,10 +5,10 @@ import math as m
 import copy as cp
 import math as math
 
-# conste3
+# params
 M = 20
-A_br = 3  # A borrow estimate
-A_rr = 3  # A return estimate
+A_br = 3  # A borrow count estimate
+A_rr = 3  # A return count estimate
 B_br = 4
 B_rr = 2
 car_value_1 = 0
@@ -16,7 +16,7 @@ car_value_2 = 0
 car_lost_penalty = 5
 car_borrow_reward = 5
 discount_rate = 0.9
-cars_move_penalty = 1
+cars_move_penalty = -1
 
 
 def poisson(pi, n): return m.pow(pi, n) / m.factorial(n) * m.pow(m.e, -pi)
@@ -46,40 +46,49 @@ def get_transitions_probability(borrow_rate, return_rate):
     rr_probs = [poisson(return_rate, i) for i in range(M + 1)]
 
     c_probs = [0 for r in range(2 * M + 1)]
+    estimated_rewards = [0 for r in range(2 * M + 1)]
 
     for i in range(M + 1):
         for j in range(M + 1):
-            c_probs[j - i + M] += br_probs[i] * rr_probs[j]
+            composite_prob = br_probs[i] * rr_probs[j]
 
-    return c_probs
+            c_probs[j - i + M] += composite_prob
+            estimated_rewards[j - i + M] += i * car_borrow_reward * composite_prob
+
+    # for i in range(2 * M + 1):
+    #     estimated_rewards[i] /= c_probs[i]
+
+    return [c_probs, estimated_rewards]
 
 
 def get_a_props_table():
 
-    a_c_probs = get_transitions_probability(A_br, A_rr)
-    b_c_probs = get_transitions_probability(B_br, B_rr)
+    a_c_probs, a_rewards = get_transitions_probability(A_br, A_rr)
+    b_c_probs, b_rewards = get_transitions_probability(B_br, B_rr)
 
     c_global_transition_probs = [[0 for i in range(2 * M + 1)] for j in range(2 * M + 1)]
+    c_global_transition_reward = [[0 for i in range(2 * M + 1)] for j in range(2 * M + 1)]
 
     for i in range(2 * M + 1):
         for j in range(2 * M + 1):
             c_global_transition_probs[i][j] = a_c_probs[i] * b_c_probs[j]
+            c_global_transition_reward[i][j] = (a_c_probs[i] * a_rewards[i] + b_c_probs[j] * b_rewards[i]) / (a_c_probs[i] + b_c_probs[j])
 
-    return c_global_transition_probs
+    return [c_global_transition_probs, c_global_transition_reward]
 
 
 def get_initial_policy():
     return [[1 for i in range(M + 1)] for j in range(M + 1)]
 
 
-def calculate_move_result(cars_to_move, cars_in_a, cars_in_b, fixed_rewards, u, transition_probs):
+def calculate_move_result(cars_to_move, cars_in_a, cars_in_b, fixed_rewards, u, transition_probs, trans_rewards):
     next_i = cars_in_a - cars_to_move
     next_j = cars_in_b + cars_to_move
 
     assert next_i >= 0
     assert next_j >= 0
 
-    acc = 0
+    acc = math.fabs(cars_to_move) * cars_move_penalty
 
     for i in range(next_i - M, next_i + M + 1):
         for j in range(next_j - M, next_j + M + 1):
@@ -89,32 +98,35 @@ def calculate_move_result(cars_to_move, cars_in_a, cars_in_b, fixed_rewards, u, 
             if 0 <= i < M + 1 and 0 <= j < M + 1:
                 current_val = u[i][j]
 
-            acc += current_val * transition_probs[i - (next_i - M)][j - (next_j - M)]
+                if i < next_i:
+                    current_val += car_borrow_reward * (next_i - i)
 
-    # if 0 <= i < M + 1 and 0 <= j < M + 1:
-    #   acc += discount_rate * u[cars_in_a][cars_in_b]
+                if j < next_j:
+                    current_val += car_borrow_reward * (next_j - j)
+
+            acc += current_val * transition_probs[i - (next_i - M)][j - (next_j - M)]
 
     return acc
 
 
-def iterate_policy(policy, fixed_state_rewards, u, trans_props_table):
+def iterate_policy(policy, fixed_state_rewards, u, trans_props_table, trans_rewards):
     next_u = [[0 for i in range(M + 1)] for j in range(M + 1)]
     next_policy = cp.deepcopy(policy)
 
     for i in range(M + 1):
         for j in range(M + 1):
-            max_cu = calculate_move_result(0, j, i, fixed_state_rewards, u, trans_props_table)
+            max_cu = calculate_move_result(0, i, j, fixed_state_rewards, u, trans_props_table, trans_rewards)
             p = 0
 
-            for k in range(1, min([i, 5 + 1])):
-                temp_u = calculate_move_result(k, i, j, fixed_state_rewards, u, trans_props_table)
+            for k in range(1, min([i, 5]) + 1):
+                temp_u = calculate_move_result(k, i, j, fixed_state_rewards, u, trans_props_table, trans_rewards)
 
                 if temp_u > max_cu:
                     max_cu = temp_u
                     p = k
 
             for k in range(max([-5, -j]), 0):
-                temp_u = calculate_move_result(k, i, j, fixed_state_rewards, u, trans_props_table)
+                temp_u = calculate_move_result(k, i, j, fixed_state_rewards, u, trans_props_table, trans_rewards)
 
                 if temp_u > max_cu:
                     max_cu = temp_u
@@ -126,13 +138,13 @@ def iterate_policy(policy, fixed_state_rewards, u, trans_props_table):
     return [next_policy, next_u]
 
 
-def get_policy(fixed_state_rewards, trans_props_table):
+def get_policy(fixed_state_rewards, trans_props_table, trans_rewards):
 
     print_u = False
 
     policy = get_initial_policy()
 
-    p, u = iterate_policy(policy, fixed_state_rewards, fixed_state_rewards, trans_props_table)
+    p, u = iterate_policy(policy, fixed_state_rewards, fixed_state_rewards, trans_props_table, trans_rewards)
 
     if print_u:
         print_float_2d(u)
@@ -141,8 +153,8 @@ def get_policy(fixed_state_rewards, trans_props_table):
 
     print(''.join(['-' for i in range(70)]))
 
-    for i in range(10):
-        p, u = iterate_policy(p, fixed_state_rewards, u, trans_props_table)
+    for i in range(15):
+        p, u = iterate_policy(p, fixed_state_rewards, u, trans_props_table, trans_rewards)
 
         if print_u:
             print_float_2d(u)
@@ -157,9 +169,14 @@ def get_policy(fixed_state_rewards, trans_props_table):
 def main():
     rewards = get_rewards_table()
     #  print(np.matrix(rewards))
-    trans_props_table = get_a_props_table()
-    #  print_2d(trans_props_table)
-    get_policy(rewards, trans_props_table)
+    trans_props, trans_rewards = get_a_props_table()
+    #  print_2d(trans_props)
+
+    print_float_2d(trans_rewards)
+
+    # return
+
+    get_policy(rewards, trans_props, trans_rewards)
 
 
 if __name__ == '__main__':
