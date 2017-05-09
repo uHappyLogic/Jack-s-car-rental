f04bef3f8baa4d0c73e6e81e135eaab172d56c63
uHappyLogic <kosiak.lukasz@gmail.com>
2017-05-10 00:35:11 +0200

    Final version

--- kosiak_117272.py
+++ kosiak_117272.py
@@ -13,10 +13,11 @@ B_br = 4
 B_rr = 2
 car_value_1 = 0
 car_value_2 = 0
-car_lost_penalty = 0
 car_borrow_reward = 100
 discount_rate = 0.9
-cars_move_penalty = 20
+cars_move_penalty = -20
+
+line = ''.join(['-' for f in range(70)])
 
 
 def poisson(pi, n): return m.pow(pi, n) / m.factorial(n) * m.pow(m.e, -pi)
@@ -33,7 +34,6 @@ def print_float_2d(ar):
 def get_initial_u():
     rewards = [
         [j * car_value_2 + i * car_value_1 for j in range(M + 1)] for i in range(M + 1)
-        # [0.1 for j in range(M + 1)] for i in range(M + 1)
     ]
 
     return rewards
@@ -62,9 +62,18 @@ def get_a_props_table():
 
     c_global_transition_probs = [[0 for i in range(2 * M + 1)] for j in range(2 * M + 1)]
 
+    print('\n'.join([str(el) for el in a_c_probs]))
+    print(line)
+
+    print('\n'.join([str(el) for el in b_c_probs]))
+    print(line)
+
     for i in range(2 * M + 1):
         for j in range(2 * M + 1):
             c_global_transition_probs[i][j] = a_c_probs[i] * b_c_probs[j]
+
+    print_float_2d(c_global_transition_probs)
+    print(line)
 
     return c_global_transition_probs
 
@@ -82,19 +91,17 @@ def calculate_move_result(cars_to_move, cars_in_a, cars_in_b, u, transition_prob
     assert next_i >= 0
     assert next_j >= 0
 
-    acc = math.fabs(cars_to_move) * -cars_move_penalty
+    acc = math.fabs(cars_to_move) * cars_move_penalty
 
     for i in range(next_i - M, next_i + M + 1):
         for j in range(next_j - M, next_j + M + 1):
-
-            current_val = -car_lost_penalty
 
             reward_val = 0
 
             temp_i = min([max([0, i]), M])
             temp_j = min([max([0, j]), M])
 
-            current_val += u[temp_i][temp_j]
+            current_val = u[temp_i][temp_j]
 
             # value smaller than current is equal to amount of borrowed cars
             if temp_i < next_i:
@@ -117,7 +124,7 @@ def iterate_policy(policy, u, trans_props_table):
         for j in range(M + 1):
 
             # we can borrow only as mny cars as we have
-            # and moving more cars that
+            # and there is no point to move more cars than station can carry
             from_a_to_b = min([i, M - j, 5])
             from_b_to_a = max([-5, -j, -(M - i)])
 
@@ -135,7 +142,10 @@ def iterate_policy(policy, u, trans_props_table):
             next_policy[i][j] = p
             next_u[i][j] = max_cu
 
-    return [next_policy, next_u]
+    policy_changed = not (np.array_equal(next_policy, policy))
+    usability_changed = not (np.array_equal(next_u, u))
+
+    return [next_policy, next_u, policy_changed, usability_changed]
 
 
 def get_policy(initial_u, trans_props):
@@ -145,29 +155,33 @@ def get_policy(initial_u, trans_props):
 
     initial_policy = get_initial_policy()
 
-    p, u = iterate_policy(initial_policy, initial_u, trans_props)
+    p, u, polict_changed, usability_changed = iterate_policy(initial_policy, initial_u, trans_props)
 
+    print(str(-1) + ' ' + line)
     if print_u:
         print_float_2d(u)
     else:
         print_number_2d(p)
 
-    print(''.join(['-' for i in range(70)]))
-
     # count of iterations
-    iterations = 50
+    iterations = 100
 
     for i in range(iterations):
-        p, u = iterate_policy(p, u, trans_props)
+        print(str(i) + ' ' + line)
+        p, u, polict_changed, usability_changed = iterate_policy(p, u, trans_props)
 
         if print_u:
             print_float_2d(u)
         else:
             print_number_2d(p)
 
-        print(''.join(['-' for i in range(70)]))
+        if polict_changed:
+            print('policy changed')
 
-    return initial_policy
+        if usability_changed:
+            print('usability changed')
+
+    return p
 
 
 def main():
